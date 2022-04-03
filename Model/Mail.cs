8223ef3e220093a6400be58715ad72e167e00259
Devon Eilers <devon.d.eilers@gmail.com>
2022-04-02 19:33:37 -0700

    naming refactor

--- Model/Mail.cs
+++ Model/Mail.cs
@@ -3,10 +3,10 @@ namespace DotnetWebUtils.Model
     public class MailServerSettings
     {
         public string FromEmailAddress { get; set; }
-        public string EnrollmentSubject { get; set; }
+        public string EmailSubject { get; set; }
         public string MailServer { get; set; }
         public int MailPort { get; set; }
-        public string EnrollmentBody { get; set; }
+        public string EmailBody { get; set; }
         public string SecureEmailCertificateOid { get; set; }
         public string EmailEncryptionAlgorithmOid { get; set; }
     }
